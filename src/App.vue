2d0beb690ec5fc0d7483ebe29c85cab26b1b08fd
Anton <tsymbal044@gmail.com>
2020-11-04 14:11:59 +0200

    functional implemented

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="main-container">
+      <CryptoList />
+    </div>
+  </div>
+</template>
+
+<script>
+  import CryptoList from "./components/CryptoList";
+
+  export default {
+    name: 'App',
+
+    components: {
+      CryptoList,
+    }
+  }
+</script>
+
+<style lang="scss">
+ * {
+   box-sizing: border-box;
+ }
+
+  body {
+    background-color: #d4e4ec;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #475c70;
+    margin: 40px 140px;
+
+  }
+</style>
